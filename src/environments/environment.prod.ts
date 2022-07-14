d7bccb9486c410fd2e4435d107c723830ba2f1d1
TuKey <pt27122000@gmail.com>
2022-07-14 22:37:44 +0700

    update config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL: 'https://tripill-api.herokuapp.com/api/v1',
+  mapboxKey:
+    'pk.eyJ1IjoicGhhbmFuaHR1IiwiYSI6ImNsNGN3cm1kZjAyaDMzcHB0M3A0Z2hpcjYifQ.37AaXkOHcU_3_d_-j7U_kQ',
 };
